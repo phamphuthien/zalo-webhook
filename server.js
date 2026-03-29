11b002f03a4d5d4c816c1d0a49377c3b2d442fb2
phamphuthien <phamphuthien0409@gmail.com>
2026-03-29 10:11:53 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/webhook", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+});
+
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+app.listen(3000);
