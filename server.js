36bd20b6eeb3c738cac33b3e67d06b9567caa23a
phamphuthien <phamphuthien0409@gmail.com>
2026-03-29 11:35:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -14,3 +14,22 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000);
+
+app.post("/webhook", async (req, res) => {
+  const userId = req.body.sender.id;
+  const message = req.body.message.text;
+
+  // gửi qua Google Sheet
+  await fetch("https://script.google.com/macros/s/AKfycbymjEle-mW3Tdpf2i8W7C0PEkZJtLguSU8WAPawMrLKerno3jn0qYywTS54eXJj5ZAbSA/exec", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      user_id: userId,
+      message: message
+    })
+  });
+
+  res.send("OK");
+});
